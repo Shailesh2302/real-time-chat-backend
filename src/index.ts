1f10737a87c74355cc5b71c0d80334383076705d
Shailesh_18_ <rushikeshkanfade123@gmail.com>
2025-08-19 23:08:05 +0530

    push

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route";
+import path from "path";
 import messageRoutes from "./routes/message.route";
 import dotenv from "dotenv";
 import connectDB from "./lib/db";
@@ -12,6 +13,7 @@ dotenv.config();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
+const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -24,6 +26,14 @@ const PORT = process.env.PORT! || 8080;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB()
   .then(() => {
