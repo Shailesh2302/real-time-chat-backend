7a652b919957112d15d92eaf6eba6683c70f7995
Rushikesh kanfade <rushikeshkanfade123@gmail.com>
2025-08-20 00:43:11 +0530

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://real-time-chat-frontend-jnfp6k37o-rush-projects.vercel.app"],
   },
 });
 
@@ -35,3 +35,4 @@ io.on("connection", (socket) => {
 });
 
 export { io, app, server };
+
